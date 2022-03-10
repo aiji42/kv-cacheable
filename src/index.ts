c8202d1a46728e86f689c998c2465a6ef9d1202f
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-10 11:36:54 +0900

    feat: flexible control of cache

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,61 @@
 type OriginalFunction<T> = (...[]: unknown[]) => T;
 
-export type KVOptions = {
-  cacheable?: boolean;
-} & KVNamespacePutOptions;
+export type CacheableOption =
+  | {
+      cacheable?: boolean;
+      expiration?: number;
+      expirationTtl?: number;
+    }
+  | boolean;
 
-export type Option = {
-  debug?: boolean
-}
+export type CacheableController<T> = (
+  arg: Awaited<T>
+) => CacheableOption | Promise<CacheableOption>;
 
-const makeKVCacheable =
-  (KV: KVNamespace, option?: Option) =>
-  async <T>(
-    org: OriginalFunction<T> | Promise<T>,
-    key: string,
-    controller?:
-      | KVOptions
-      | ((arg: Awaited<T>) => KVOptions | Promise<KVOptions>)
-  ): Promise<T> => {
-    const cache = await KV.get<T>(key, "json");
+export type CommonOption = {
+  debug?: boolean;
+  expiration?: number;
+  expirationTtl?: number;
+};
+
+export type CacheableWrapper = <T>(
+  key: string,
+  originalFunction: OriginalFunction<T> | Promise<T>,
+  option?: CacheableOption | CacheableController<T>
+) => Promise<T>;
+
+type InferResult<T> = T extends OriginalFunction<infer U>
+  ? U
+  : T extends Promise<infer U>
+  ? U
+  : never;
+
+const makeCacheable =
+  (KV: KVNamespace, option?: CommonOption): CacheableWrapper =>
+  async (key, org, controller) => {
+    const cache = await KV.get<InferResult<typeof org>>(key, "json");
     if (cache) {
       option?.debug && console.log("cache hit: ", key);
       return cache;
     }
 
     const result = await (typeof org === "function" ? org() : org);
-    const { cacheable = true, ...kvOption } =
+    const cacheableOption =
       typeof controller === "function"
         ? await controller(result)
         : controller ?? {};
+    const cacheable =
+      typeof cacheableOption === "object"
+        ? cacheableOption.cacheable ?? true
+        : cacheableOption;
     if (cacheable) {
-      await KV.put(key, JSON.stringify(result), kvOption);
+      await KV.put(key, JSON.stringify(result), {
+        ...option,
+        ...(typeof cacheableOption === "object" ? cacheableOption : {}),
+      });
       option?.debug && console.log("cache set: ", key);
     }
     return result;
   };
 
-export default makeKVCacheable;
+export default makeCacheable;
